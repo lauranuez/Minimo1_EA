2d806801b3b5a9e80fee75c73d95026fcd5c9d7f
lauranuez <lauranuezzaragoza@gmail.com>
2021-11-12 12:00:42 +0100

    first

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { FormComponent } from './components/form/form.component';
-import { PreviewComponent } from './components/preview/preview.component';
-import { ListComponent } from './components/list/list.component';
-import { AddAsignaturaComponent } from './components/add-asignatura/add-asignatura.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
-    FormComponent,
-    PreviewComponent,
-    ListComponent,
-    AddAsignaturaComponent
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
